5c6a3333201df29659bd2e12f05947025ab6607d
fairoos <fairoosnm123@gmail.com>
2019-07-09 22:35:59 +0530

    Adds basic templates

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('<h1>Blog Home<h1>')
+    return render(request, 'blog/home.html')
 
 def about(request):
-    return HttpResponse('<h1>Blog about<h1>')
+    return render(request, 'blog/about.html')
 

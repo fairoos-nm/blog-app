2afb40ea4d49d3172c681e6d29780d82b26293d3
fairoos-nm <fairoosnm123@gmail.com>
2019-07-24 19:25:35 +0530

    Adds login and logout

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.
 from django.contrib import messages
 from .form import UserRegisterForm
 
@@ -10,10 +11,12 @@ def register(request):
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
-            user_name = form.cleaned_data.get('username')
-            messages.success(request, f'accounted created for {user_name}!')
-            return redirect('blog-home')
+            #user_name = form.cleaned_data.get('username')
+            messages.success(request, f'Your accounted is created for and You can login now')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form':form})
     
+def profile(request):
+    return render(request, 'users/profile.html')

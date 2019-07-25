5dceb10bf7e9c869e290556243a3a6924a65e47c
fairoos <fairoosnm123@gmail.com>
2019-07-25 10:16:07 +0530

    Adds login required

--- users/views.py
+++ users/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.
+from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .form import UserRegisterForm
 
@@ -17,6 +17,7 @@ def register(request):
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form':form})
-    
+
+@login_required
 def profile(request):
     return render(request, 'users/profile.html')
